e7d0d6bb180778a6eddc6948ebb21cd8feed4cb7
jan <janalbertdriessen@gmail.com>
2026-09-22 12:05:11 +0200

    Add push-relay APNs registration (PushNotifications.swift, entitlements)

--- Relay/RelayApp.swift
+++ Relay/RelayApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct RelayApp: App {
+    @UIApplicationDelegateAdaptor(PushAppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .task {
+                    PushNotifications.requestAuthorizationAndRegister()
+                }
         }
     }
 }
